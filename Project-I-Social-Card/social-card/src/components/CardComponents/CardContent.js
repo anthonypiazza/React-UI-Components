c73e8262b93c9addae8326c000abf1bb727c2d3d
anthonypiazza <piazza.anthony.n@gmail.com>
2019-05-26 09:58:27 -0500

    Completed Project I

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div>
+            <div className='content-title'>
+                <h3> Get started with React</h3>
+            </div>
+            <div className='content-body'>
+                <p>React makes it painess to create interactive UIs. 
+                   Design simple views for each state in your application</p>
+            </div>
+            <div className='content-link'>
+                <p>reactjs.org</p>
+            </div>
+        </div>
+    );
+};
+
+export default CardContent;
