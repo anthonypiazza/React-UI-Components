38984f16a585a6c1ba26e1b4562373b11a74102a
anthonypiazza <piazza.anthony.n@gmail.com>
2019-05-28 15:35:08 -0500

    Completed Project II

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Display.css';
+import ActionButton from '../ButtonComponents/ActionButton';
+import NumberButton from '../ButtonComponents/NumberButton';
+
+const CalculatorDisplay = () =>{
+    return(
+        <div className='calculator-display'>
+            <h1>0</h1>
+        </div>
+    )
+}
+
+
+export default CalculatorDisplay;
