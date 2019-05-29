254783478a045e7a5fc87aab42e1ec6640d14d60
anthonypiazza <piazza.anthony.n@gmail.com>
2019-05-28 19:07:03 -0500

    Project II edits

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './Display.css';
-import ActionButton from '../ButtonComponents/ActionButton';
-import NumberButton from '../ButtonComponents/NumberButton';
 
 const CalculatorDisplay = () =>{
     return(
